7fa698e8b2b1b7b8a6ce0e80b76b6909c929f0f7
GrgSumin <be2019se673@gces.edu.np>
2022-01-04 22:37:23 +0545

    done

---
+++ Assignments/Assignment4/GUI/InheritanceFrame.java
@@ -0,0 +1,52 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+class InheritanceFrame extends JFrame 
+{
+	public static void main(String[] args)
+	{
+		InheritanceFrame frame = new InheritanceFrame();
+        JLabel number1 = new JLabel();
+        JTextField number1text = new JTextField();
+        JLabel number2 = new JLabel();
+        JTextField number2text = new JTextField();
+        JLabel result = new JLabel();
+        JTextField resulttext = new JTextField();
+        JButton addition = new JButton("Add");
+        JButton subtraction = new JButton("Subtract");
+        JButton multiply = new JButton("multiply");
+        JButton divide = new JButton("Divide");
+        
+        number1.setText("Number 1:");
+        number1.setBounds(10,60,75,25);
+        number1text.setBounds(100,60,180,25);
+        frame.add(number1);
+        frame.add(number1text);
+        number2.setText("Number 2: ");
+        number2.setBounds(10,60,75,25);
+        number2text.setBounds(100,60,180,25);
+        frame.add(number2);
+        frame.add(number2text);
+        result.setText("Answer:");
+        result.setBounds(10,60,75,25);
+        resulttext.setBounds(100,60,180,25);
+        frame.add(result);
+        frame.add(resulttext);
+        addition.setBounds(10,200,50,35);
+        frame.add(addition);
+        subtraction.setBounds(80,200,50,35);
+        frame.add(subtraction); 
+        multiply.setBounds(150,200,50,35);
+        frame.add(multiply);
+        divide.setBounds(220,200,50,35);
+        frame.add(divide);
+        frame.setSize(300,400);
+        
+        frame.setLayout(null);
+        frame.setVisible(true);
+       
+	}
+
+}
+
